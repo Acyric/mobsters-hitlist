4aa8faaae2745b0c20f858a7535bfff518a441fd
dave92082 <dave92082@05e8b89c-906b-11dd-a517-6d4a4eef63e0>
2008-10-02 10:30:57 +0000

    Initial Build

---
+++ mobsters-hitlist/mobsters-hitlist/frmMain.vb
@@ -0,0 +1,46 @@
+﻿Public Class frmMain
+    'Very crude at the moment...I only spent about 5 min on it and it works
+    'for now. Alot more to come later!
+    Public Declare Auto Function SetCursorPos Lib "User32.dll" (ByVal X As Integer, ByVal Y As Integer) As Long
+    Public Declare Auto Function GetCursorPos Lib "User32.dll" (ByRef lpPoint As Point) As Long
+    Public Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
+    Public Const MOUSEEVENTF_LEFTDOWN = &H2 ' left button down
+    Public Const MOUSEEVENTF_LEFTUP = &H4 ' left button up
+    Public Const MOUSEEVENTF_MIDDLEDOWN = &H20 ' middle button down
+    Public Const MOUSEEVENTF_MIDDLEUP = &H40 ' middle button up
+    Public Const MOUSEEVENTF_RIGHTDOWN = &H8 ' right button down
+    Public Const MOUSEEVENTF_RIGHTUP = &H10 ' right button up
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        tRefresh.Interval = 500
+        tRefresh.Enabled = True
+        tRefresh.Start()
+        'MsgBox(MousePosition.X.ToString + " " + MousePosition.Y.ToString)
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tRefresh.Tick
+        SetCursorPos(-306, 257)
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        Dim I As Integer
+        Do While I < 1500
+            I += 0.75
+        Loop
+        I = 0
+        SetCursorPos(-270, 495)
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        Do While I < 1500
+            I += 0.75
+        Loop
+        I = 0
+        SetCursorPos(-270, 495)
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    End Sub
+    Protected Overrides Function ProcessCmdKey(ByRef msg As System.Windows.Forms.Message, ByVal keyData As System.Windows.Forms.Keys) As Boolean
+        Select Case keyData
+            Case Keys.Control Or Keys.Alt Or Keys.Shift Or Keys.G
+                End
+
+        End Select
+    End Function
+End Class
